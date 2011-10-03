d567250b3fe02bf1c2d21332eeda98f3fc2edd92
Miguel Guerreiro <miguel.guerreiro@gmail.com>
2011-10-03 16:25:24 +0100

    First working version

---
+++ README.md
@@ -0,0 +1,8 @@
+jQuery.charLimit
+===
+
+jQuery plugin to limit the number of characters allowed in a textarea input, displaying the remaining characters countdown at the bottom of the textarea element (text turns red when reaching the limit)
+
+Usage:
+---
+	$('textarea').charLimit(500);
--- jquery.charLimit.js
+++ jquery.charLimit.js
@@ -1,42 +1,60 @@
 /**
- *	Limit text input
+ *	Limit a textarea input to the allowed number of charaters (limit)
+ *	displays the information of the remaining characters allowed, counting
+ *	down as the user types; text turns red when the limit is about to be
+ *	reached, text input beyond the limit is automatically removed
  *	Author: 	Miguel Guerreiro
  *	Updated:	2011-10-03
  */
 
 ;(function() {
-	$.fn.charLimit = function(limit) {
-		var me = this,
-			myId = me.attr('id'),
-			info = $('<span/>',{id:myId+'-charsleft',css:{fontSize:'10px',color:'#9f9'}}),
-			// get the character limit
-			chars = me.val().length,
-			limit = limit || 500;
+	$.fn.charLimit = function(limit,text) {
+		var me		= this,
+			myId	= me.attr('id'),
+			// feedback HTML
+			info	= $('<span/>',{id:myId+'-charsleft',css:{width:(me.width() - 10),fontSize:'10px',textAlign:'right',color:'#9f9'}}),
+			// check the input for existing content's length
+			chars	= me.val().length,
+			// text to append after the countdown number
+			text	= text || ' remaining',
+			// maximum number of characters allowed
+			limit	= limit || 500;
+		// onFocus event
 		me.focus(function() {
+			// position the feedback HTML
 			info.css({
 				position:	'absolute',
 				top:		me.position().top + me.height() - 15,
 				left:		me.position().left + 5
-			}).html((limit - chars) + ' remaining');
+			}).html((limit - chars) + text);
+			// display the feedback HTML
 			me.after(info);
 		})
+		// onBlur event
 		.blur(function() {
+			// hide the feedback HTML
 			info.remove();
 		})
+		// onKeyUp event
 		.keyup(function() {
-			var text = me.val();
-			chars = text.length;
+			// get the current content of the textarea
+			var content = me.val();
+			// update the content's length
+			chars = content.length;
 			// don't allow more characters than the limit
 			if (chars > limit) {
 				chars = limit;
-				me.val(text.substr(0,limit));
+				// cap the content to the specified limit
+				me.val(content.substr(0,limit));
 			}
+			// show feedback text in red if reaching the limit, otherwise, text is green
 			if (limit - chars < 10) {
 				info.css({color:'#f99'});
 			} else {
 				info.css({color:'#9f9'});
 			}
-			info.html((limit-chars) + ' remaining');
+			// update the feedback HTML
+			info.html((limit-chars) + text);
 		});
 	};
 })(jQuery);
